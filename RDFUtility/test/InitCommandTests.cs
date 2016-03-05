3276dc4e5e85328607c097f5bac128a31ed28a5d
Justin Emgarten <justin@emgarten.com>
2016-03-05 12:45:19 -0800

    Moving NuGet.Data to this repo

---
+++ RDFUtility/test/InitCommandTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Emgarten.Test
+{
+    public class InitCommandTests
+    {
+        [Fact]
+        public async Task InitCommand_Basic()
+        {
+            using (var target = new TestFolder())
+            using (var cache = new LocalCache())
+            {
+                // Arrange
+                var log = new TestLogger();
+                var fileSystem = new PhysicalFileSystem(cache, new Uri(target.Root));
+                var settings = new LocalSettings();
+
+                var indexJsonOutput = new FileInfo(Path.Combine(target.Root, "index.json"));
+                var settingsOutput = new FileInfo(Path.Combine(target.Root, "Emgarten.settings.json"));
+                var autoCompleteOutput = new FileInfo(Path.Combine(target.Root, "autocomplete", "query"));
+                var catalogOutput = new FileInfo(Path.Combine(target.Root, "catalog", "index.json"));
+                var searchOutput = new FileInfo(Path.Combine(target.Root, "search", "query"));
+
+                // Act
+                var exitCode = await InitCommandTestHook.RunCore(settings, fileSystem, log);
+
+                // Assert
+                Assert.Equal(0, exitCode);
+                Assert.True(indexJsonOutput.Exists);
+                Assert.True(settingsOutput.Exists);
+                Assert.True(autoCompleteOutput.Exists);
+                Assert.True(catalogOutput.Exists);
+                Assert.True(searchOutput.Exists);
+            }
+        }
+    }
+}
