d3e7f5cd0470c49f53ee97295fa7c1e479eeb811
Justin Emgarten <justin@emgarten.com>
2017-02-07 15:12:09 -0800

    Updating sources, removing old code

---
+++ TestProjectGraphGenerator/TestProjectGraphGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestProjectGraphGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
